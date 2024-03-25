19a8fa3762853899430f4a572b15346a082c1c97
amanda <amanda.piva01@fatec.sp.gov.br>
2024-03-24 21:34:37 -0300

    Finalizando ladinPage

--- src/LadinPage/LadinPage.jsx
+++ src/LadinPage/LadinPage.jsx
@@ -2,12 +2,16 @@ import Box from "@mui/material/Box";
 import pcdCadeiraRodas from "../../public/pcdCadeiraRodas.png";
 import Logo from "../../public/logo.png";
 import IdosoCriancas from "../../public/idosoECriancas.png";
+import Menina from "../../public/menina.png";
+import IdososLendo from "../../public/idososLendo.png";
+import IdososFelizes from "../../public/idososFelizes.png";
 import { Typography } from "@mui/material";
 import "@fontsource/montserrat/300.css";
 import "@fontsource/montserrat/400.css";
 import "@fontsource/montserrat/500.css";
 import "@fontsource/montserrat/700.css";
 import Button from "@mui/material/Button";
+import Link from "@mui/material/Link";
 
 function LadinPage() {
   return (
@@ -92,7 +96,7 @@ function LadinPage() {
                   color: "#04BFAF",
                   fontWeight: "600",
                   padding: "3rem 5rem",
-
+                  fontFamily: "montserrat",
                   marginTop: "5vh",
                 }}
               >
@@ -104,6 +108,7 @@ function LadinPage() {
                   color: "#E64097",
                   fontWeight: "600",
                   paddingLeft: "5rem",
+                  fontFamily: "montserrat",
                 }}
               >
                 Levando as pessoas a praticarem mais a caridade, ajudando PcDs,
@@ -115,6 +120,7 @@ function LadinPage() {
                   color: "#231F20",
                   paddingLeft: "5rem",
                   marginTop: "10vh",
+                  fontFamily: "montserrat",
                 }}
               >
                 Para pessoas que precisam de equipamentos de deficientes ou
@@ -130,6 +136,248 @@ function LadinPage() {
               style={{ padding: "8rem", margin: "auto" }}
               alt="idosoCriancas"
             />
+          </Box>
+
+          {/*SEGUNDA COLUNA */}
+          <Box
+            sx={{ display: "flex", flexDirection: "row", marginBottom: "5vh" }}
+          >
+            <img
+              width={300}
+              height={300}
+              src={Menina}
+              style={{ marginLeft: "55vw" }}
+              alt="menina"
+            />
+
+            <img
+              width={200}
+              height={200}
+              src={IdososLendo}
+              style={{ marginLeft: "8vw" }}
+              alt="idososLendo"
+            />
+          </Box>
+        </Box>
+
+        {/*SESSÃO SOBRE A PLATAFORMA */}
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            backgroundColor: "#04BFAF",
+          }}
+        >
+          <Typography
+            sx={{
+              fontSize: "36px",
+              fontWeight: "600",
+              color: "#FFFFFF",
+              margin: "18vh auto",
+              fontFamily: "montserrat",
+            }}
+          >
+            Conheça mais sobre nós
+          </Typography>
+
+          <Box
+            sx={{ display: "flex", flexDirection: "row", marginBottom: "5vh" }}
+          >
+            {/*LADO DIREITO */}
+            <Box sx={{ width: "50vw" }}>
+              <Box sx={{ width: "30vw", marginLeft: "auto", padding: "3em" }}>
+                <Typography
+                  sx={{
+                    fontSize: "24px",
+                    color: "#FFFFFF",
+                    fontWeight: "300",
+                    fontFamily: "montserrat",
+                  }}
+                >
+                  Somos uma nova plataforma que nasceu da visão de muita
+                  necessidade de PcDs e idosos que dependiam de doações de
+                  comunidades e igrejas que se disponibilizavam a ajudar, e com
+                  isso, a ideia de fornecer as fraldas em conjunto com os
+                  equipamentos de maneira rápida e tecnológica veio a tona
+                </Typography>
+              </Box>
+            </Box>
+
+            {/*LADO ESQUERDO */}
+            <Box sx={{ width: "50vw" }}>
+              <Box sx={{ width: "30vw", marginRight: "auto", padding: "3em" }}>
+                <Typography
+                  sx={{
+                    fontSize: "24px",
+                    color: "#FFFFFF",
+                    fontWeight: "300",
+                    fontFamily: "montserrat",
+                  }}
+                >
+                  Depois de muita pesquisa e coleta de dados sobre as
+                  necessidades dessas pessoas, a missão de ajudar está mais
+                  fácil de ser executada.
+                </Typography>
+              </Box>
+            </Box>
+          </Box>
+        </Box>
+
+        {/*SESSÃO CADASTRO E LOGIN */}
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            width: "100vw",
+            height: "100vh",
+            backgroundImage: `linear-gradient(to bottom,  #04BFAF, rgba(230, 64, 151, 0)), url(${IdososFelizes})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+        >
+          <Typography
+            sx={{
+              fontSize: "36px",
+              fontWeight: "600",
+              color: "#FFFFFF",
+              fontFamily: "montserrat",
+              margin: "5vh auto",
+            }}
+          >
+            Gostou da iniciativa?
+          </Typography>
+
+          <Box sx={{ width: "40vw", margin: "5vh auto" }}>
+            <Typography
+              sx={{
+                fontSize: "24px",
+                fontWeight: "600",
+                color: "#FFFFFF",
+                fontFamily: "montserrat",
+              }}
+            >
+              Venha fazer parte também e ajude alguém próximo a você ou faça sua
+              requisição se cadastrando na plataforma
+            </Typography>
+          </Box>
+
+          <Box sx={{ margin: "5vh auto" }}>
+            <Button
+              variant="contained"
+              sx={{
+                backgroundColor: "#E64097",
+                color: "#FFFFFF",
+                width: "200px",
+                "&:hover": {
+                  backgroundColor: "#04BFAF", // Altere a cor desejada para o efeito hover
+                },
+              }}
+            >
+              Cadastre-se
+            </Button>
+          </Box>
+          <Box sx={{ margin: "5vh auto" }}>
+            <Typography
+              sx={{
+                fontSize: "36px",
+                fontWeight: "600",
+                color: "#FFFFFF",
+                fontFamily: "montserrat",
+              }}
+            >
+              Se você já faz parte desta jornada, faça seu login
+            </Typography>
+          </Box>
+
+          <Box sx={{ margin: "5vh auto" }}>
+            <Button
+              variant="contained"
+              sx={{
+                backgroundColor: "#04BFAF",
+                color: "#FFFFFF",
+                width: "200px",
+                "&:hover": {
+                  backgroundColor: "#E64097", // Altere a cor desejada para o efeito hover
+                },
+              }}
+            >
+              Login
+            </Button>
+          </Box>
+        </Box>
+
+        {/*FOOTER */}
+        <Box
+          sx={{
+            backgroundColor: "#E64097",
+            width: "100vw",
+            height: "50vh",
+            display: "flex",
+            flexDirection: "row",
+          }}
+        >
+          {/*LADO DIREITO */}
+          <Box sx={{ width: "30vw", marginLeft: "auto", padding: "3em" }}>
+            <Typography
+              sx={{
+                fontSize: "36px",
+                fontWeight: "600",
+                color: "#FFFFFF",
+                fontFamily: "montserrat",
+              }}
+            >
+              Contato
+            </Typography>
+
+            {/*LINK EMAIL */}
+            <Link
+              href="#"
+              sx={{
+                color: "#FFFFFF",
+                fontFamily: "montserrat",
+                marginTop: "3vh",
+              }}
+            >
+              maos.estendidas@gmail.com
+            </Link>
+          </Box>
+
+          {/*LADO ESQUERDO */}
+          <Box sx={{ width: "30vw", marginRight: "auto", padding: "3em" }}>
+            <Typography
+              sx={{
+                fontSize: "36px",
+                fontWeight: "600",
+                color: "#FFFFFF",
+                fontFamily: "montserrat",
+              }}
+            >
+              Mídias sociais
+            </Typography>
+
+            {/*LINKS SOCIAL MEDIA */}
+            <Box sx={{ display: "flex", flexDirection: "column" }}>
+              <Link
+                href="#"
+                sx={{
+                  color: "#FFFFFF",
+                  fontFamily: "montserrat",
+                  marginTop: "3vh",
+                }}
+              >
+                @maos.estendidas
+              </Link>
+              <Link
+                href="#"
+                sx={{
+                  color: "#FFFFFF",
+                  fontFamily: "montserrat",
+                  marginTop: "3vh",
+                }}
+              >
+                Maos Estendidas
+              </Link>
+            </Box>
           </Box>
         </Box>
       </Box>
